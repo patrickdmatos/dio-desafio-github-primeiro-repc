ad33cf1fa82b5d4e82eee7dff5c5a3ed556fdb27
patrickdmatos <patrick.1101reis@gmail.com>
2022-06-08 23:39:19 -0300

    contador1.0

--- Modulo 3 Javascript/assets/js/script.js
+++ Modulo 3 Javascript/assets/js/script.js
@@ -5,25 +5,11 @@ function decrement(){
     span = document.getElementById('currentNumber');
     currentNumber = currentNumber - 1;
     span.innerHTML = currentNumber;
-
-    if(currentNumber < 0){
-        span = document.getElementById('currentNumber').style.color = 'red';
-    }
-    else if(currentNumber >= 0){
-        span = document.getElementById('currentNumber').style.color = 'black';
-    }
 }
 function increment(){
     span = document.getElementById('currentNumber');
     currentNumber = currentNumber + 1;
     span.innerHTML = currentNumber;
-    
-    if(currentNumber < 0){
-        span = document.getElementById('currentNumber').style.color = 'red';
-    }
-    else if(currentNumber >= 0){
-        span = document.getElementById('currentNumber').style.color = 'black';
-    }
 }
 
 // MUDA COR DO TEXTO QUANDO NEGATIVO
