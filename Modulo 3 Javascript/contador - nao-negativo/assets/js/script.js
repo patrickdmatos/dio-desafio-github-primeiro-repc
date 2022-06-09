ad396c691f0464362e2219d4eb4903a246feff87
patrickdmatos <patrick.1101reis@gmail.com>
2022-06-09 19:12:57 -0300

    contadores-finalizados

--- Modulo 3 Javascript/contador - nao-negativo/assets/js/script.js
+++ Modulo 3 Javascript/contador - nao-negativo/assets/js/script.js
@@ -3,11 +3,15 @@ var currentNumber = 0;
 
 function decrement(){
     span = document.getElementById('currentNumber');
+    if(currentNumber >= 1){
         currentNumber = currentNumber - 1;
+    }else{
+        currentNumber = currentNumber;
+    }
     span.innerHTML = currentNumber;
-};
+}
 function increment(){
     span = document.getElementById('currentNumber');
     currentNumber = currentNumber + 1;
     span.innerHTML = currentNumber;
-};
+}
---
+++ Modulo 3 Javascript/contador-muda-cor/assets/js/script.js
@@ -0,0 +1,29 @@
+var span = 0;
+var currentNumber = 0;
+
+function decrement(){
+    span = document.getElementById('currentNumber');
+    currentNumber = currentNumber - 1;
+    span.innerHTML = currentNumber;
+
+    if(currentNumber < 0){
+        span = document.getElementById('currentNumber').style.color = 'red';
+    }
+    else if(currentNumber >= 0){
+        span = document.getElementById('currentNumber').style.color = 'black';
+    }
+}
+function increment(){
+    span = document.getElementById('currentNumber');
+    currentNumber = currentNumber + 1;
+    span.innerHTML = currentNumber;
+    
+    if(currentNumber < 0){
+        span = document.getElementById('currentNumber').style.color = 'red';
+    }
+    else if(currentNumber >= 0){
+        span = document.getElementById('currentNumber').style.color = 'black';
+    }
+}
+
+
